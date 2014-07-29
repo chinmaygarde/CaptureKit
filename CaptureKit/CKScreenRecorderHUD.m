25f39337ba3aed42610676a7fa11123753d92141
Chinmay Garde <chinmay@apportable.com>
2014-07-29 02:21:11 -0700

    Enable panning the controls view

--- CaptureKit/CKScreenRecorderHUD.m
+++ CaptureKit/CKScreenRecorderHUD.m
@@ -19,7 +19,9 @@ static const CGFloat CKScreenRecorderHUDInset = 10.0;
 
 @end
 
-@implementation CKScreenRecorderHUD
+@implementation CKScreenRecorderHUD {
+    CGPoint _startingLocationInPan;
+}
 
 -(void) performCommonScreenRecorderHUDInitialization {
     self.windowLevel = UIWindowLevelNormal + 1;
@@ -37,6 +39,9 @@ static const CGFloat CKScreenRecorderHUDInset = 10.0;
     self.controls.recorder = self.recorder;
 
     [self addSubview:self.controls];
+    
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPanControls:)];
+    [self.controls addGestureRecognizer:recognizer];
     
     [self windowDimensionsNeedUpdate];
 }
@@ -79,6 +84,27 @@ static const CGFloat CKScreenRecorderHUDInset = 10.0;
     controls.frame = CGRectMake((boundsSize.width - controlsSize.width) / 2.0,
                                  boundsSize.height - controlsSize.height - CKScreenRecorderHUDInset,
                                  controlsSize.width, controlsSize.height);
+}
+
+-(void) onPanControls:(UIPanGestureRecognizer *) pan {
+    switch (pan.state) {
+        case UIGestureRecognizerStatePossible:
+            break;
+        case UIGestureRecognizerStateBegan:
+            _startingLocationInPan = self.controls.center;
+            break;
+        case UIGestureRecognizerStateChanged:
+        {
+            CGPoint translation = [pan translationInView:self];
+            self.controls.center = CGPointMake(_startingLocationInPan.x + translation.x, _startingLocationInPan.y + translation.y);
+        }
+            break;
+        case UIGestureRecognizerStateEnded:
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateFailed:
+            _startingLocationInPan = CGPointZero;
+            break;
+    }
 }
 
 -(void) windowDimensionsNeedUpdate {
